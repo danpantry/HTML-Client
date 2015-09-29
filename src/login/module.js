a7b1a2f6664f788becad0750e5422590f6f82ce4
SirPython <SirPython@users.noreply.github.com>
2015-09-29 19:55:36 -0400

    reverted module since I don't know how to handle this

--- src/login/module.js
+++ src/login/module.js
@@ -4,15 +4,11 @@ var ngRoute = require('angular-route');
 var LoginController = require('./controller');
 var template = require('./login.html');
 var serverInterface = require('../server_interface/module');
-var serverListFactory = require("./servers_list");
-var currentUser = require("./current_user");
 
 module.exports = angular.module('cardshifter.login', [ngRoute, serverInterface.name])
-    .config(function($routeProvider) {
-        $routeProvider.when('/', {
-            controller: LoginController,
-            template: template
-        });
-    })
-    .factory("ServerList", serverListFactory)
-    .factory("CurrentUser", currentUser);
+  .config(function($routeProvider) {
+    $routeProvider.when('/', {
+      controller: LoginController,
+      template: template
+    });
+  });
